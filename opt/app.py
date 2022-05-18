d150907dcd0207c978071b56c02e6ae939b14237
土屋健 <tata.5593@gmail.com>
2022-05-18 18:00:41 +0900

    testっていうファイルを作りました

--- opt/app.py
+++ opt/app.py
@@ -13,11 +13,3 @@ st.sidebar.title('学習か予測かを選んでください')
 selection = st.sidebar.radio("選択", list(PAGES.keys()))
 page = PAGES[selection]
 page.app()
-
-#エクスパンだー
-# expander1 = st.beta_expander("モデルの精度が上がりません")
-# expander1.write("データに欠損値がないのか、確認してください")
-# expander2 = st.beta_expander("データが読み込みません")
-# expander2.write("拡張子がcsvになっているのか確認してください")
-# expander3 = st.beta_expander("その他")
-# expander3.write("こちらまで連絡ください")
